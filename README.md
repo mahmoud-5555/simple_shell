f5a28f177f7f1fbf2a2c9f7dbde2d4f89004b6c0
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-11-02 22:41:01 +0200

    first commit - first push

---
+++ README.md
@@ -0,0 +1,3 @@
+# this is the simple shell project in C
+## we are doing hard things  
+## i will push as match as passable
---
+++ main.c
@@ -0,0 +1,15 @@
+#include 'main.h'
+
+/**
+ * main - the main process of the program well be in <main()>
+ * @argc:_________
+ * @argv:_________
+ * Return:________
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+
+	return (0); /*every think going well*/
+}
